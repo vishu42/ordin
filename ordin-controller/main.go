e177166b87965cf76634c727b1549624f28d7a08
Vishal Tewatia <tewatiavishal10@gmail.com>
2024-07-28 12:38:01 +0530

    init

---
+++ ordin-controller/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	//
+	// Uncomment to load all auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth"
+	//
+	// Or uncomment to load specific auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+)
+
+func main() {
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Create the shared informer factory and use the client to connect to
+	// Kubernetes
+	factory := informers.NewSharedInformerFactory(clientset, 0)
+
+	// Get the informer for the right resource, in this case a Pod
+	informer := factory.Core().V1().Pods().Informer()
+
+	// Create a channel to stops the shared informer gracefully
+	stopper := make(chan struct{})
+	defer close(stopper)
+
+	// Kubernetes serves an utility to handle API crashes
+	defer runtime.HandleCrash()
+
+	// This is the part where your custom code gets triggered based on the
+	// event that the shared informer catches
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		// When a new pod gets created
+		AddFunc: func(obj interface{}) { fmt.Println("New pod created") },
+	})
+
+	// You need to start the informer, in my case, it runs in the background
+	go informer.Run(stopper)
+}
